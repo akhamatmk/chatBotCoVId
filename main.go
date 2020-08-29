da2aad3414db53fd0eab33c38c0a1d58b6363412
akhamat_mhealthbank <akhamat.mk@mhealthbank.com>
2020-08-29 19:34:06 +0700

    commit this

--- main.go
+++ main.go
@@ -2,55 +2,36 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
-	"time"
 
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
 func main() {
+	var (
+		port      = os.Getenv("PORT")
+		publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
+		token     = os.Getenv("TOKEN")      // you must add it to your config vars
+	)
 
-	b, err := tb.NewBot(tb.Settings{
-		// You can also set custom API URL.
-		// If field is empty it equals to "https://api.telegram.org".
-		URL: "http://195.129.111.17:8012",
+	webhook := &tb.Webhook{
+		Listen:   ":" + port,
+		Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
+	}
 
-		Token:  "TOKEN_HERE",
-		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
-	})
+	pref := tb.Settings{
+		Token:  token,
+		Poller: webhook,
+	}
 
+	b, err := tb.NewBot(pref)
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
 
 	b.Handle("/hello", func(m *tb.Message) {
-		b.Send(m.Sender, "Hello World!")
+		b.Send(m.Sender, "Hi!")
 	})
 
 	b.Start()
-
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
-
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
-
-	router.GET("/ping", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
-
-	router.Run(":" + port)
 }
