f8a8795b807e5c7d55eb5c52b8ee91eabf7bafba
akhamat_mhealthbank <akhamat.mk@mhealthbank.com>
2020-08-29 19:29:07 +0700

    commit this

--- main.go
+++ main.go
@@ -4,12 +4,35 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
+	tb "gopkg.in/tucnak/telebot.v2"
 )
 
 func main() {
+
+	b, err := tb.NewBot(tb.Settings{
+		// You can also set custom API URL.
+		// If field is empty it equals to "https://api.telegram.org".
+		URL: "http://195.129.111.17:8012",
+
+		Token:  "TOKEN_HERE",
+		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/hello", func(m *tb.Message) {
+		b.Send(m.Sender, "Hello World!")
+	})
+
+	b.Start()
+
 	port := os.Getenv("PORT")
 
 	if port == "" {
